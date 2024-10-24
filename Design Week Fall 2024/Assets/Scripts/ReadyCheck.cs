3435fb78b6954cc0d874f5a9c7770191a9f8cc63
JakedeVos <jakedevos4@gmail.com>
2024-10-24 16:59:11 -0400

    Ready Check Sprites Created

--- Design Week Fall 2024/Assets/Scripts/ReadyCheck.cs
+++ Design Week Fall 2024/Assets/Scripts/ReadyCheck.cs
@@ -7,7 +7,13 @@ public class ReadyCheck : MonoBehaviour
 {
     public bool p1Ready = false;
     public bool p2Ready = false;
+    public GameObject ready1Off, ready1On, ready2Off, ready2On;
 
+    private void Start()
+    {
+        ready1On.SetActive(false);
+        ready2On.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,10 +21,15 @@ public class ReadyCheck : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             p1Ready = true;
+            ready1Off.SetActive(false);
+            ready1On.SetActive(true);
+
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             p2Ready = true;
+            ready2Off.SetActive(false);
+            ready2On.SetActive(true);
         }
 
         if (p1Ready && p2Ready)
